a3f5f56f2df61bb11548380e59db275d81ae2958
Vasiliy <zubarevprimary@gmail.com>
2020-01-14 22:14:53 +0300

    prostoAd

---
+++ Assets/Scripts/MenuAppearance.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+using Vuforia;
+
+public class MenuAppearance : DefaultTrackableEventHandler
+{
+    public GameObject menu;
+    public GameObject trigger;
+    public GameObject mainMenu;
+    protected override void OnTrackingFound()
+    {
+        if (mainMenu.activeSelf == false && trigger.activeSelf == false)
+        {
+            base.OnTrackingFound();
+            trigger.SetActive(true);
+            menu.SetActive(true);
+        }
+    }
+}
